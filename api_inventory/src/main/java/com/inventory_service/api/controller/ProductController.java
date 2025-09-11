f3bdf7b0090f6bbad31c5c857dcc1e9b1863b804
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-10 23:30:24 -0300

    feat: created product controller

--- api_inventory/src/main/java/com/inventory_service/api/controller/ProductController.java
+++ api_inventory/src/main/java/com/inventory_service/api/controller/ProductController.java
@@ -1,4 +1,68 @@
 package com.inventory_service.api.controller;
 
+import com.inventory_service.api.dto.CreateProductDTO;
+import com.inventory_service.api.dto.ResponseProductDTO;
+import com.inventory_service.api.service.ProductServices;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("products")
 public class ProductController {
+    private final DefaultTransactionDefinition defaultTransactionDefinition;
+    private ProductServices productServices;
+
+    public ProductController(ProductServices productServices, DefaultTransactionDefinition defaultTransactionDefinition) {
+        this.productServices = productServices;
+        this.defaultTransactionDefinition = defaultTransactionDefinition;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<ResponseProductDTO> create(@RequestBody CreateProductDTO data) {
+        var newProduct = productServices.createProduct(data);
+
+        var location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newProduct.id())
+                .toUri();
+
+        return ResponseEntity.status(HttpStatus.CREATED).location(location).body(newProduct);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<ResponseProductDTO>> getProducts(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+
+        Pageable pageable = PageRequest.of(page, size);
+        var  products = this.productServices.getAllProducts(pageable);
+
+        return ResponseEntity.ok().body(products);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ResponseProductDTO> getProductForId(@PathVariable String id) {
+        var  products = this.productServices.getProductForId(id);
+        return ResponseEntity.ok().body(products);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<?> getHealth() {
+        return ResponseEntity.ok().body(
+                Map.of(
+                        "status", true,
+                        "message", "Product service is up!"
+                )
+        );
+    }
 }
