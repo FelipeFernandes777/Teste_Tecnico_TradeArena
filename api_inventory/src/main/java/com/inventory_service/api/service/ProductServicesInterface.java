7d72a9ab2dac27ec1fe5f6c4c7c24effd329d242
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-10 22:33:17 -0300

    feat: created interface to modelation my product services

--- api_inventory/src/main/java/com/inventory_service/api/service/ProductServicesInterface.java
+++ api_inventory/src/main/java/com/inventory_service/api/service/ProductServicesInterface.java
@@ -1,4 +1,14 @@
 package com.inventory_service.api.service;
 
+import com.inventory_service.api.dto.CreateProductDTO;
+import com.inventory_service.api.dto.ResponseProductDTO;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductServicesInterface {
+    ResponseProductDTO createProduct(CreateProductDTO data);
+    List<ResponseProductDTO> getAllProducts();
+    Optional<ResponseProductDTO> getProductForId(String productId);
+    void health();
 }
