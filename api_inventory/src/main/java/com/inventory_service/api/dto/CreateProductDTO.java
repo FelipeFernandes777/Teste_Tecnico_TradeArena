bc7bafbb95ac8076dc7ba2dc8315e2dbade64b6b
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-11 19:39:17 -0300

    change: Anotations before @NotNull after @NotBlank

--- api_inventory/src/main/java/com/inventory_service/api/dto/CreateProductDTO.java
+++ api_inventory/src/main/java/com/inventory_service/api/dto/CreateProductDTO.java
@@ -1,18 +1,18 @@
 package com.inventory_service.api.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CreateProductDTO(
-        @NotNull
+        @NotBlank
         String sku,
-        @NotNull
-        @Min(1)
+        @NotBlank
         String name,
         @NotNull
         @Min(1)
         Float price,
-        @NotNull
+        @Min(0)
         int stock
 ) {
 }
