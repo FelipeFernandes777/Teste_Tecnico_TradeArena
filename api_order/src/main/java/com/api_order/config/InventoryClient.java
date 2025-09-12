d750135e8233b4b3c558a1265c7df0688e3a52ee
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-12 19:48:34 -0300

    feat: created a Inventory Client Class, a generic method to send request and one method to verify if inventory api is up

---
+++ api_order/src/main/java/com/api_order/config/InventoryClient.java
@@ -0,0 +1,65 @@
+package com.api_order.config;
+
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+
+@Service
+public class InventoryClient {
+
+    private final WebClient webClient;
+    private final int maxAttempts = 3;
+    private final Duration requestTimeout = Duration.ofSeconds(5);
+
+    public InventoryClient(WebClient.Builder builder) {
+        HttpClient httpClient = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(3));
+
+        this.webClient = builder
+                .baseUrl("http://localhost:8080/products")
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+    }
+
+    // Método genérico para qualquer requisição
+    public <T> T request(HttpMethod method, String uri, Object body, Class<T> responseType) {
+        int attempts = 0;
+
+        while (attempts < maxAttempts) {
+            try {
+                WebClient.RequestBodySpec requestSpec = webClient.method(method).uri(uri);
+
+                if (body != null) {
+                    requestSpec.bodyValue(body);
+                }
+
+                return requestSpec
+                        .retrieve()
+                        .bodyToMono(responseType)
+                        .block(requestTimeout);
+            } catch (Exception e) {
+                attempts++;
+                if (attempts >= maxAttempts) {
+                    throw new RuntimeException("FAILED to call a API, after " + maxAttempts + " attempts", e);
+                }
+                try {
+                    Thread.sleep((long) Math.pow(2, attempts) * 1000); // backoff exponencial
+                } catch (InterruptedException ignored) {}
+            }
+        }
+
+        return null;
+    }
+
+    public Boolean verifyIfInventoryApiIsUp() {
+        try {
+            return request(HttpMethod.GET, "/health", null, Boolean.class);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
