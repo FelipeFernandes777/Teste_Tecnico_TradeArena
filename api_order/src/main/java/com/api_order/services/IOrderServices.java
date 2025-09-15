59fb5c9c1d6d5faba82f50b09f9ff71d932cba91
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-15 14:25:08 -0300

    feat: created method to create order in my service

--- api_order/src/main/java/com/api_order/services/IOrderServices.java
+++ api_order/src/main/java/com/api_order/services/IOrderServices.java
@@ -1,6 +1,7 @@
 package com.api_order.services;
 
-import com.api_order.dto.ResponseOrderDTO;
+import com.api_order.dto.order.CreateOrderRequest;
+import com.api_order.dto.order.ResponseOrderDTO;
 import org.aspectj.apache.bcel.classfile.Unknown;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,7 +9,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.UUID;
 
 public interface IOrderServices {
-    ResponseOrderDTO createOrder(Unknown data);
+    ResponseOrderDTO createOrder(CreateOrderRequest data);
     ResponseOrderDTO getOrderForId(UUID orderId);
     Page<ResponseOrderDTO> getOrders(Pageable pageable);
     void cancelOrder(UUID orderId);
--- api_order/src/main/java/com/api_order/services/OrderServices.java
+++ api_order/src/main/java/com/api_order/services/OrderServices.java
@@ -1,16 +1,23 @@
 package com.api_order.services;
 
 import com.api_order.config.InventoryClient;
-import com.api_order.dto.ResponseOrderDTO;
+import com.api_order.dto.inventory.ResponseProductDTO;
+import com.api_order.dto.item.OrderItemRequest;
+import com.api_order.dto.order.CreateOrderRequest;
+import com.api_order.dto.order.ResponseOrderDTO;
+import com.api_order.dto.order.StockDetail;
 import com.api_order.model.order.OrderModel;
+import com.api_order.model.order.OrderStatus;
+import com.api_order.model.orderItem.OrderItemModel;
 import com.api_order.repository.OrderRepository;
 import jakarta.transaction.Transactional;
-import org.aspectj.apache.bcel.classfile.Unknown;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 //TODO criar exceptions and exception handler
@@ -27,8 +34,55 @@ public class OrderServices implements IOrderServices{
 
     @Override
     @Transactional
-    public ResponseOrderDTO createOrder(Unknown data) {
-        return null;
+    public ResponseOrderDTO createOrder(CreateOrderRequest data) {
+        if(!this.inventoryClient.verifyIfInventoryApiIsUp()){
+            throw new RuntimeException("Inventory service is unavailable");
+        }
+
+        BigDecimal total = BigDecimal.ZERO;
+        List<StockDetail> stockProblems =  new ArrayList<>(); // SotckDetail
+        List<OrderItemModel> orderItems =  new ArrayList<>();
+
+        for(OrderItemRequest item: data.items()) {
+            try {
+                ResponseProductDTO product = this.inventoryClient.getProduct(item.productId()); // Percore a lista e pega o item com o id informado
+
+                if (product.stock() < item.qty()) {
+                    stockProblems.add(new StockDetail(
+                            item.productId(),
+                            item.qty(),
+                            product.stock()
+                    ));
+                }
+
+                this.inventoryClient.updateStock(item.productId(), -item.qty()); // Já pega o stock do produto e subtrai pela quantidade informada
+
+                BigDecimal itemTotal = product.price().multiply(BigDecimal.valueOf(item.qty())); // Pega o valor do item e já multiplica pela quantidade
+                total = total.add(itemTotal);
+
+                OrderItemModel orderItem = new OrderItemModel();
+                orderItem.setProduct_id(item.productId());
+                orderItem.setQty(item.qty());
+                orderItem.setUnitPrice(product.price());
+
+                orderItems.add(orderItem);
+
+            } catch (RuntimeException e) {
+                throw new RuntimeException("Error processing product: " + item.productId(), e);
+            }
+        }
+        if (!stockProblems.isEmpty()) {
+            throw new RuntimeException("Insufficient stock");
+        }
+
+        OrderModel order = new OrderModel();
+        order.setStatus(OrderStatus.CREATED);
+        order.setTotal(total);
+        order.setItems(orderItems);
+        order.setCreatedAt(LocalDateTime.now());
+
+        OrderModel savedOrder = orderRepository.save(order);
+        return new ResponseOrderDTO(savedOrder);
     }
 
     @Override
