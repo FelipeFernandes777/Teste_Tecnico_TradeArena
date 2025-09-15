a9146fa97fdca8e656c4b9ac060260ae8cea7add
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-15 19:43:40 -0300

    feat: Add a attributes for response order DTO

--- api_order/src/main/java/com/api_order/dto/order/ResponseOrderDTO.java
+++ api_order/src/main/java/com/api_order/dto/order/ResponseOrderDTO.java
@@ -1,6 +1,22 @@
 package com.api_order.dto.order;
 
-public record ResponseOrderDTO(
+import com.api_order.model.order.OrderModel;
+import com.api_order.model.order.OrderStatus;
+import com.api_order.model.orderItem.OrderItemModel;
 
-        com.api_order.model.order.OrderModel order) {
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public record ResponseOrderDTO(
+            UUID id,
+            OrderStatus status,
+            BigDecimal total,
+            List<OrderItemModel> items,
+            LocalDateTime createdAt
+        ) {
+    public ResponseOrderDTO(OrderModel raw){
+        this(raw.getId(),raw.getStatus(),raw.getTotal(),raw.getItems(),raw.getCreatedAt());
+    }
 }
