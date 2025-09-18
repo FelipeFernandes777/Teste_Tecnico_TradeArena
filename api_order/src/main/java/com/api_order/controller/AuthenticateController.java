a70b03243c8b9b0e59c83cb6fc37b5c60b3270da
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-09-18 12:15:40 -0300

    feat: Created local a generate token

--- api_order/src/main/java/com/api_order/controller/AuthenticateController.java
+++ api_order/src/main/java/com/api_order/controller/AuthenticateController.java
@@ -1,4 +1,42 @@
 package com.api_order.controller;
 
-public class AutenticateController {
+import com.api_order.config.security.FixedJwtGenerator;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthenticateController {
+
+    private FixedJwtGenerator fixedJwtGenerator;
+    public AuthenticateController(FixedJwtGenerator fixedJwtGenerator) {
+        this.fixedJwtGenerator = fixedJwtGenerator;
+    }
+
+    @GetMapping("/generate")
+    public ResponseEntity<?> generateToken(){
+        try{
+            var token = this.fixedJwtGenerator.generateToken("inventory");
+            return ResponseEntity.ok().body(
+                    Map.of(
+                            "token", token,
+                            "status", "success",
+                            "statusCode", HttpStatus.OK
+                    )
+            );
+        } catch (RuntimeException e) {
+            return ResponseEntity.unprocessableEntity().body(
+                    Map.of(
+                            "message", e.getMessage(),
+                            "status", "success",
+                            "statusCode", HttpStatus.UNPROCESSABLE_ENTITY
+                    )
+            );
+        }
+    }
 }
